895ef508fee1f8d6201761004683a8fb13855769
Satyanjal <satyanjal.prakash@analyttica.com>
2020-08-07 21:54:03 +0530

    added customer entity

---
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/CustomerEntity.java
@@ -0,0 +1,106 @@
+package com.upgrad.FoodOrderingApp.service.entity;
+
+import org.apache.commons.lang3.builder.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "customer")
+@NamedQueries(
+        {
+                //@NamedQuery(name = "answerByUuid", query = "select a from AnswerEntity a where a.uuid = :uuid")
+        }
+)
+
+public class CustomerEntity {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "uuid", unique = true)
+    @Size(max = 200)
+    @NotNull
+    private String uuid;
+
+    @Column(name = "firstname")
+    @Size(max = 30)
+    @NotNull
+    private String firstName;
+
+    @Column(name = "lastname")
+    @Size(max = 30)
+    private String lastName;
+
+    @Column(name = "email")
+    @Size(max = 50)
+    private String email;
+
+    @Column(name = "contact_number", unique = true)
+    @Size(max = 30)
+    @NotNull
+    private String contactNumber;
+
+    @Column(name = "password")
+    @Size(max = 255)
+    @NotNull
+    private String password;
+
+    @Column(name = "salt")
+    @NotNull
+    @Size(max = 200)
+    @ToStringExclude
+    private String salt;
+
+    @Override
+    public boolean equals(Object obj) {
+        return new EqualsBuilder().append(this, obj).isEquals();
+    }
+
+    public Long getId() {return id;}
+
+    public void setId(Long id) {this.id = id;}
+
+    public String getUuid() {return uuid;}
+
+    public void setUuid(String uuid) {this.uuid = uuid;}
+
+    public String getFirstName() {return firstName;}
+
+    public void setFirstName(String firstName) {this.firstName = firstName;}
+
+    public String getLastName() {return lastName;}
+
+    public void setLastName(String lastName) {this.lastName = lastName;}
+
+    public String getEmail() {return email;}
+
+    public void setEmail(String email) {this.email = email;}
+
+    public String getContactNumber() {return contactNumber;}
+
+    public void setContactNumber(String contactNumber) {this.contactNumber = contactNumber;}
+
+    public String getPassword() {return password;}
+
+    public void setPassword(String password) {this.password = password;}
+
+    public String getSalt() {return salt;}
+
+    public void setSalt(String salt) {this.salt = salt;}
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(this).hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
+}
+
+
