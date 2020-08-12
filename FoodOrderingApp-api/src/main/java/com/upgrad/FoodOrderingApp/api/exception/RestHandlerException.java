f27c308cd4db9b237ebb9b157bd35f73a9c67988
swatitoshniwal <swati.toshniwal2@gmail.com>
2020-08-12 13:38:30 +0530

    AuthenticationFailedException added to the exception.
    Signed-off-by: swatitoshniwal <swati.toshniwal2@gmail.com>

--- FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/exception/RestHandlerException.java
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/exception/RestHandlerException.java
@@ -1,6 +1,7 @@
 package com.upgrad.FoodOrderingApp.api.exception;
 
 import com.upgrad.FoodOrderingApp.api.model.ErrorResponse;
+import com.upgrad.FoodOrderingApp.service.exception.AuthenticationFailedException;
 import com.upgrad.FoodOrderingApp.service.exception.SignUpRestrictedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,8 +15,12 @@ public class RestHandlerException {
     @ExceptionHandler(SignUpRestrictedException.class)
     public ResponseEntity<ErrorResponse> signUpRestrictedException (SignUpRestrictedException exc, WebRequest request){
         return new ResponseEntity<ErrorResponse>(new ErrorResponse()
-                .code(exc.getCode())
-                .message(exc.getErrorMessage()),
-                HttpStatus.BAD_REQUEST);
+                .code(exc.getCode()).message(exc.getErrorMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(AuthenticationFailedException.class)
+    public  ResponseEntity<ErrorResponse> authenticationFailedException (AuthenticationFailedException exc, WebRequest request) {
+        return new ResponseEntity<ErrorResponse>(new ErrorResponse()
+                .code(exc.getCode()).message(exc.getErrorMessage()), HttpStatus.UNAUTHORIZED);
     }
 }
